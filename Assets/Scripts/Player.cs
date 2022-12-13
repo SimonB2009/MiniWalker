bea37410d19bdbcca15a7f643148a03cbb01537d
SimonB2009 <brandstetter.simon@web.de>
2022-12-13 19:42:54 +0100

    fix: fixed last jumping bug

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 8f;
+    float jumpForce = 70f; //hoehe
+    Rigidbody2D m_Rigidbody;
+
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {    
+        //MOVEMENT
+        var movement = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(movement,0) * Time.deltaTime * speed);
+        //m_Rigidbody.AddForce(new Vector2(movement * speed, 0));
+
+        if (Input.GetKey(KeyCode.Space)) {
+            //m_Rigidbody.AddForce(transform.up );
+            m_Rigidbody.AddForce(new Vector2(0, jumpForce));
+        }  
+        
+    }
+}
