5b1c43cade1eac92d994b9ba7048304cf6437094
SimonB2009 <brandstetter.simon@web.de>
2022-12-21 11:33:31 +0100

    feat: added first ground check

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,20 @@ using System.Collections.Generic;
 public class Player : MonoBehaviour
 {
     float speed = 8f;
-    float jumpForce = 70f; //hoehe
+    float jumpForce = 100f; //hoehe
     float selectnummberA = 0;
     float selectnummberD = 0;
     Rigidbody2D m_Rigidbody;
-    //public Animator animator;
+    private BoxCollider2D coll;
+    //blic Animator animator;
     //public CharacterController2D controller;
+    [SerializeField] private LayerMask jumpableGround;
 
 
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         selectnummberA++;                   //turn test
         Rotate(1,selectnummberA); //left
         selectnummberD++;
@@ -24,22 +27,28 @@ public class Player : MonoBehaviour
 
     private void Update()
     {   
+        isGrounded();
         //MOVEMENT
         var movement = Input.GetAxis("Horizontal");
         transform.Translate(new Vector2(movement,0) * Time.deltaTime * speed);
-        //m_Rigidbody.AddForce(new Vector2(movement * speed, 0));
+
+        //animator.SetFloat("speed", Mathf.Abs(movement));
 
         if (Input.GetKey(KeyCode.Space)) {
-            //m_Rigidbody.AddForce(transform.up );
-            m_Rigidbody.AddForce(new Vector2(0, jumpForce));
+            if (isGrounded() == true) {
+                //animator.SetBool("jumping", true);
+                m_Rigidbody.AddForce(new Vector2(0, jumpForce));
+            }
         } 
 
         if (Input.GetKeyDown(KeyCode.A)) {
+            //animator.SetBool("jumping", false);
             selectnummberA++;
             Rotate(1,selectnummberA); //left
         }   
 
         if (Input.GetKeyDown(KeyCode.D)) {
+            //animator.SetBool("jumping", false);
             selectnummberD++;
             Rotate(0,selectnummberD); //right
         } 
@@ -47,19 +56,7 @@ public class Player : MonoBehaviour
     }
 
      void Rotate (float rotation, float selectnummber) { //0=right 1=left
-        /*Quaternion theRotation = transform.localRotation;
-         
-        if (rotation == 0) {
-            theRotation.y = 178;
-            rotation = 1;
-        }
-        if (rotation == 1) {
-            theRotation.y = 0;
-            rotation = 0;
-        }
-
-        transform.localRotation = theRotation;*/
-
+       
         if (rotation == 0) { //rechtsa
             selectnummberA = 0;
             speed = 8f;
@@ -76,4 +73,9 @@ public class Player : MonoBehaviour
             } 
         }
      }
+
+    public bool isGrounded() {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0, Vector2.down, .1f, jumpableGround);
+    }
+
 }
