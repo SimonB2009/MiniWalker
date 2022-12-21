d8b30e8660db49dcfebd8a56b8e4c7854e592356
SimonB2009 <brandstetter.simon@web.de>
2022-12-21 17:09:16 +0100

    feat: added first controller

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    float speed = 8f; //should be the same as the player speed
+    Rigidbody2D m_Rigidbody;
+
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {    
+        //MOVEMENT
+        var movement = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(movement,0) * Time.deltaTime * speed);
+        //m_Rigidbody.AddForce(new Vector2(movement * speed, 0));
+
+        /*if (Input.GetKey(KeyCode.Space)) {
+            //m_Rigidbody.AddForce(transform.up );
+            m_Rigidbody.AddForce(new Vector2(0, jumpForce));
+        }*/  
+        
+    }
+}
